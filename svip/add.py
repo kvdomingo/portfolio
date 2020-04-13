be17aa97c7004e7429f01c0753f1ecf343330614
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-14 03:44:38 +0800

    fix: removed dead reference links, added meta keywords

---
+++ svip/add.py
@@ -0,0 +1,22 @@
+from csv import reader
+from svip.models import BlogPost
+
+
+with open('./static/svip/data/blog186.csv', 'r', encoding='utf-8') as f:
+    data = reader(f, delimiter=',')
+    for row in data:
+        head = [
+            'created',
+            'modified',
+            'subject_id',
+            'title',
+            'body',
+            'cover',
+            'slug',
+            'status',
+            'keywords'
+        ]
+        data_dict = dict((head, data[1:]))
+        b = BlogPost(**data_dict)
+        b.save()
+        print(f'Uploaded {b}')
