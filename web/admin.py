299c77b87f60c20453167e8fd6797d5565e663d0
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-07-13 02:15:10 +0800

    add CV models

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from .models import *
 
+
+admin.site.register(Education)
+admin.site.register(Work)
+admin.site.register(Project)
+admin.site.register(Certification)
+admin.site.register(Publication)
+admin.site.register(Reference)
 
 admin.site.index_title = 'Admin'
 admin.site.site_title = 'Kenneth V. Domingo'
