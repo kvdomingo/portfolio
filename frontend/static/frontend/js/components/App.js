05e0e59bc616b54266bd05cbabebfb2e1e6ed22a
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-10-07 00:38:02 +0800

    add FullPageLoading for App Suspense

--- frontend/static/frontend/js/components/App.js
+++ frontend/static/frontend/js/components/App.js
@@ -1,11 +1,10 @@
-import React, { Suspense } from 'react';
-import AOS from 'aos';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Routes from './Routes';
-import Loading from './Loading';
-
+import React, { Suspense } from "react";
+import AOS from "aos";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Routes from "./Routes";
+import Loading from "./FullPageLoading";
 
 export default class App extends React.Component {
     componentDidMount() {
@@ -16,9 +15,7 @@ export default class App extends React.Component {
         return (
             <Router>
                 <Navbar />
-                <Suspense fallback={<Loading />}>
-                    {Routes}
-                </Suspense>
+                <Suspense fallback={<Loading />}>{Routes}</Suspense>
                 <Footer />
             </Router>
         );
---
+++ frontend/static/frontend/js/components/FullPageLoading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { MDBContainer as Container } from "mdbreact";
+
+export default function FullPageLoading() {
+    return (
+        <Container
+            className="text-center my-5 mt-5"
+            style={{ height: "100vh" }}
+        >
+            <div className="my-5 mt-5 spinner-grow spinner-grow-lg d-inline-block" />
+        </Container>
+    );
+}
