c5690e50ac2006770d918b532d2dc3798a94d341
Kenneth V. Domingo <hello@kvd.studio>
2023-11-19 16:01:22 +0800

    feat: convert AP186-9

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,58 +1,61 @@
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
-import remarkMath from "remark-math";
+import { defineConfig } from "astro/config";
 import rehypeMathJax from "rehype-mathjax";
+import remarkMath from "remark-math";
 
 const mathjaxConfig = () =>
-  rehypeMathJax({
-    tex: {
-      tags: "ams",
-    },
-  });
+	rehypeMathJax({
+		tex: {
+			tags: "ams",
+		},
+	});
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  integrations: [
-    tailwind(),
-    sitemap(),
-    svelte({
-      include: ["**/svelte/*"],
-    }),
-    react({
-      include: ["**/react/*"],
-    }),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "material-theme-ocean",
-      },
-      gfm: true,
-      remarkPlugins: [remarkMath],
-      rehypePlugins: [mathjaxConfig],
-    }),
-  ],
-  output: "server",
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-  vite: {
-    resolve: {
-      alias: {
-        "@": "src",
-      },
-    },
-  },
-  site: "https://kvd.studio",
-  adapter: vercel({
-    speedInsights: {
-      enabled: true,
-    },
-  }),
+	prefetch: true,
+	integrations: [
+		tailwind(),
+		sitemap(),
+		svelte({
+			include: ["**/svelte/*"],
+		}),
+		react({
+			include: ["**/react/*"],
+		}),
+		mdx({
+			syntaxHighlight: "shiki",
+			shikiConfig: {
+				theme: "material-theme-ocean",
+			},
+			gfm: true,
+			remarkPlugins: [remarkMath],
+			rehypePlugins: [mathjaxConfig],
+		}),
+	],
+	output: "server",
+	server: {
+		host: "0.0.0.0",
+		port: 3000,
+	},
+	vite: {
+		resolve: {
+			alias: {
+				"@": "src",
+			},
+		},
+	},
+	site: "https://kvd.studio",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+		speedInsights: {
+			enabled: true,
+		},
+	}),
 });
