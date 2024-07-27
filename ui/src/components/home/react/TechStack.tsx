77a2f9776aa827248f74291859fda59515087c70
Kenneth V. Domingo <hello@kvd.studio>
2024-07-27 20:22:42 +0800

    fix: responsiveness on some pages

--- ui/src/components/home/react/TechStack.tsx
+++ ui/src/components/home/react/TechStack.tsx
@@ -11,7 +11,7 @@ export default function TechStack() {
             <div className="flex place-content-center place-items-center border-b border-solid border-white pb-6 pr-0 text-center md:place-content-end md:border-b-0 md:border-r md:pb-0 md:pr-6 md:text-right">
               <h5 className="section-header text-lg">{header}</h5>
             </div>
-            <div className="col-span-3 my-auto grid grid-cols-3 gap-4 md:grid-cols-6 md:gap-12">
+            <div className="col-span-3 my-auto grid grid-cols-4 gap-4 md:grid-cols-6 md:gap-12">
               {technologies.map(tech => (
                 <TechStackItem key={tech.name} name={tech.name} logo={tech.logo} />
               ))}
