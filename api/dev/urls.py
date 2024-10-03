44968fab9f33b380ce3fd639b03b82fa434d2069
Kenneth V. Domingo <hello@kvd.studio>
2024-10-03 15:57:24 +0800

    refactor: remove legacy code; move new app to root

--- api/dev/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ProjectView
-
-app_name = "dev"
-
-urlpatterns = [
-    path("project", ProjectView.as_view()),
-]
--- api/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py createsuperuser --noinput || true
-
-exec gunicorn --bind 0.0.0.0:$PORT --config gunicorn.conf.py
