e88d35d7418e768685d5d670d49590c3af5312fa
Kenneth V. Domingo <hello@kvd.studio>
2024-10-13 19:11:54 +0800

    chore: analytics in prod only

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,11 @@
 /// <reference types="astro/client" />
 
 declare module "juxtaposejs/build/js/juxtapose.min.js";
+
+interface ImportMeta {
+  readonly PUBLIC_POSTHOG_TOKEN: string;
+}
+
+interface ImportMetaEnv {
+  readonly env: ImportMeta;
+}
